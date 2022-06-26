77075a56bbbcf3d85cb65c970d83832a6028393b
Richard <isit_ric@hotmail.com>
2022-06-26 21:43:20 +0100

    css tweaks on header bar

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const yargs = require("yargs");
 const {sequelize} = require("./db/connection");
-const {addMovie,listMovie} = require("./movie/functions");
+const {addMovie,listMovie, updateMovie, deleteMovie} = require("./movie/functions");
 const {addActor,listActor} = require("./actor/functions");
 
 const app = async (yargsObj)=>{
@@ -11,19 +11,18 @@ const app = async (yargsObj)=>{
         // Check that first key is movie
         if (yargsObj.movie) {
 
+            // Add movie (and actor)
             if (yargsObj.add) {
-                await addMovie({
-                    title: yargsObj.title,
-                    ActorId: yargsObj.actorId
-                });
-                
-
+                await addMovie({title: yargsObj.title, ActorId: yargsObj.actorId});
+            // List movie and actor
             } else if (yargsObj.list) {
                 await listMovie();
 
             } else if (yargsObj.update) {
+                await updateMovie ({ title: yargsObj.newTitle}, {title: yargsObj.title});
 
             } else if (yargsObj.delete) {
+                await deleteMovie ({title: yargsObj.title});
 
             } else {
                 console.log("A: Incorrect command");
@@ -51,6 +50,8 @@ app(yargs.argv);
 // COMMANDS: MOVIE
 // LIST: node src/app.js --movie --add --title "movie1" --actorId "1"
 // LIST: node src/app.js --movie --list
+// UPDATE: node src/app.js --movie --update --title "movie1" --newTitle "movie3"
+// DELETE: node src/app.js --movie --delete --title "movie1"
 
 // COMMANDS: ACTOR
 // ADD: node src/app.js --actor --add --actorName "actor3"
--- src/movie/functions.js
+++ src/movie/functions.js
@@ -31,34 +31,34 @@ exports.listMovie = async () => {
     }
 }
 
-// exports.updMovie = async (updateObj, filterObj) => {
-//     try {
-//       //find a movie and update a column
-//       const response = await Movie.update(updateObj, { where: filterObj })
-//       if (response[0] > 0) {
-//         console.log("Successfully updated");
-//       } else {
-//         console.log("Something went wrong");
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
+exports.updateMovie = async (updateObj, filterObj) => {
+    try {
+      //find a movie and update a column
+      const response = await Movie.update(updateObj, { where: filterObj })
+      if (response[0] > 0) {
+        console.log("Successfully updated");
+      } else {
+        console.log("Something went wrong");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-// // Delete Movie
-// exports.delMovie = async (filterObj) => {
-//     try {
-//         const response = await Movie.destroy({
-//             where: {
-//                 title: filterObj.title
-//             }
-//         });
-//         if (response > 0){
-//             console.log("Successfully deleted")
-//         } else {
-//             console.log("Something went wrong");
-//         }
-//     } catch (error) {
-//         console.log(error);
-//     }
-// }
+// Delete Movie
+exports.deleteMovie = async (movieObj) => {
+    try {
+        const response = await Movie.destroy({
+            where: {
+                title: movieObj.title
+            }
+        });
+        if (response > 0){
+            console.log("Successfully deleted")
+        } else {
+            console.log("Something went wrong");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
